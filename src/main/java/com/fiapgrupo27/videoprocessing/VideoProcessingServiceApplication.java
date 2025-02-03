1d78423f570978fc0ac6985d4b9028009cb68a10
grupo27-6soat-fiap <grupo27.6soat.fiap@gmail.com>
2025-02-03 19:56:05 -0300

    Gradel

--- src/main/java/com/fiapgrupo27/videoprocessing/VideoProcessingServiceApplication.java
+++ src/main/java/com/fiapgrupo27/videoprocessing/VideoProcessingServiceApplication.java
@@ -1,9 +1,12 @@
 package com.fiapgrupo27.videoprocessing;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@EnableRabbit
 public class VideoProcessingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(VideoProcessingServiceApplication.class, args);
